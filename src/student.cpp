55912741e67d06794b6be7baac32a396c6c8457b
EricVitolo <57659508+EricVitolo@users.noreply.github.com>
2020-11-29 20:10:39 -0800

    Add files via upload

---
+++ src/student.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <fstream>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+using namespace std;
+
+void getAssignments(void *sockfd)
+{
+    int sock = *(int*)sockfd;
+    char buffer[4096];
+    bzero(buffer, 4096);
+
+    send(sock, "GET /server HTTP/1.1", sizeof("GET /server HTTP/1.1") , 0); 
+    read(sock, buffer, sizeof(buffer));
+
+    printf("%s\n", buffer );
+}
+
+void getGrade(void *sockfd, string assignmentname, string studentname)
+{
+    int sock = *(int*)sockfd;
+    char buffer[4096];
+    bzero(buffer, 4096);
+
+    string str = "GET /server/" + assignmentname + " " + "?name=" + studentname + " HTTP/1.1";
+
+    char strbuffer[(str.length() + 1)];
+    strcpy(strbuffer, str.c_str());
+    send(sock, strbuffer, sizeof(strbuffer) , 0); 
+    read(sock, buffer, sizeof(buffer));
+
+    printf("%s\n", buffer);
+}
+
+void postAssignment(void *sockfd, string assignmentname, string studentname, string filetext)
+{
+    int sock = *(int*)sockfd;
+    char buffer[4096];
+    bzero(buffer, 4096);
+
+    string str = "POST /server/" + assignmentname + " " + "?name=" + studentname + " HTTP/1.1\n" + filetext;
+
+    char strbuffer[(str.length() + 1)];
+    strcpy(strbuffer, str.c_str());
+    send(sock, strbuffer, sizeof(strbuffer) , 0); 
+    read(sock, buffer, sizeof(buffer));
+
+    printf("%s\n", buffer);
+}
+   
+int main() 
+{ 
+    int sockfd; 
+    struct sockaddr_in saddr;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    saddr.sin_family = AF_INET; 
+    saddr.sin_port = htons(5000);   
+    inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr);
+
+
+    //get user input command
+
+    
+    connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr));
+    
+    //getAssignments(&sockfd);
+    //getGrade(&sockfd, assignmentname, studentname);
+    //postAssignment(&sockfd, assignmentname, studentname, filetext);
+    
+    return 0; 
+} 
